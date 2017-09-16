cf44bab5100913960968194381330a3b3ef8c69d
dkh215 <danielkhunter@gmail.com>
2017-09-16 09:18:31 -0400

    Replace hard coded db table names with env variables.  Remove query and muation files.  Update zappa config

--- service/__init__.py
+++ service/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS, cross_origin
 import boto3
@@ -9,11 +10,11 @@ session = boto3.Session()
 db_r = session.resource('dynamodb')
 db_c = session.client('dynamodb')
 
-packages_table = db_r.Table('pkg_radar_dev_packages')
-package_tags_table = db_r.Table('pkg_radar_dev_package_tags')
-package_recommendations_table = db_r.Table('pkd_radar_dev_package_recommendations')
-user_kanban_packages_table = db_r.Table('pkg_radar_dev_user_kanban_packages')
-user_connections_table = db_r.Table('pkg_radar_dev_user_connections')
-users_table = db_r.Table('pkg_radar_dev_users')
+packages_table = db_r.Table(os.environ.get('packages_table'))
+package_tags_table = db_r.Table(os.environ.get('package_tags_table'))
+package_recommendations_table = db_r.Table(os.environ.get('package_recommendations_table'))
+user_kanban_packages_table = db_r.Table(os.environ.get('user_kanban_packages_table'))
+user_connections_table = db_r.Table(os.environ.get('user_connections_table'))
+users_table = db_r.Table(os.environ.get('users_table'))
 
 from service import views
